72b2e85975c3c096ab95b162ce8edc5629ce751d
Nguyen Quang THO <thonqFX17090@funix.edu.vn>
2022-07-16 22:02:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Badge, Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponents";
+import "./App.css";
+import { DISHES } from "./shered/dishes";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+export default App;
