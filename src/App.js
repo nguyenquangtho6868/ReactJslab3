81cf4bd26e96c37b827ffd20552c7ba19408124d
nguyenquangtho6868 <105498060+nguyenquangtho6868@users.noreply.github.com>
2022-07-18 20:24:53 +0700

    Add files via upload
    up load Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
-import { Badge, Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./components/MenuComponents";
+import Main from "./components/MainComponent";
+
 import "./App.css";
-import { DISHES } from "./shered/dishes";
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-    };
-  }
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+      <div>
+        <Main />
       </div>
     );
   }
